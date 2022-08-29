7b7dd62b3c3bf4dbfb751dd8c584e4b87fdddbf7
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2022-08-29 21:21:16 +0300

    22-ANN

--- 22-ANN/ann.py
+++ 22-ANN/ann.py
@@ -17,8 +17,8 @@ print(veriler)
 
 #veri on isleme
 
-X = veriler.iloc[:,3:13].values
-Y = veriler.iloc[:,13].values
+X = veriler.iloc[:,3:13].values 
+Y = veriler.iloc[:,13].values 
 
 
 #encoder: Kategorik -> Numeric
@@ -61,4 +61,32 @@ sc=StandardScaler()
 
 X_train = sc.fit_transform(x_train)
 X_test = sc.fit_transform(x_test)
+
+#Yapay Sinir Ağı(ANN)
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+#gizli katman oluşturma
+classifier.add(Dense(6, kernel_initializer = "uniform", activation = "relu", input_dim = 11)) #gizli katman sayısı, dağılım(uniform), aktivasyon fonksiyonları(relu), input_dim(input verilerimiz)  vs.  
+
+classifier.add(Dense(6, kernel_initializer = "uniform", activation = "relu")) #ikinci gizli katman
+
+classifier.add(Dense(1, kernel_initializer = "uniform", activation = "sigmoid")) #çıkış katmanı
+
+classifier.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+
+classifier.fit(X_train, y_train, epochs=50)
+
+y_pred = classifier.predict(X_test)
+
+y_pred = (y_pred > 0.5) #True'lar 1, False'lar 0 döndürücek, daha net olsun diye, yüzdelik değerlerdense
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
 
