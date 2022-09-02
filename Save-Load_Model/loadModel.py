57ad33cf40491b68c1f1c384999b5f9004723cbe
Recep İlyasoğlu <rcp.ilyasoglu@gmail.com>
2022-09-02 16:49:20 +0300

    Save-Load_Model

--- Save-Load_Model/loadModel.py
+++ Save-Load_Model/loadModel.py
@@ -7,7 +7,6 @@ Created on Fri Sep  2 16:43:32 2022
 
 import pandas as pd
 
-
 url = "http://bilkav.com/satislar.csv"
 
 veriler = pd.read_csv(url)
@@ -18,6 +17,7 @@ Y = veriler[:,1]
 
 bolme = 0.33
 
+
 from sklearn import model_selection
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size = bolme)
 
@@ -26,5 +26,4 @@ import pickle
 
 #load
 yuklenen = pickle.load(open("model.kayit","rb"))
-print("Yuklenen")
 print(yuklenen.predict(X_test))
